fd9ae0017fe719edd37516a3061e42fb07e25d79
冉力 <ranly314@gmail.com>
2017-03-20 09:21:40 +0800

    Context从static改为弱引用

--- RichEditText/src/main/java/com/github/runly/richedittext/utils/ContextUtils.java
+++ RichEditText/src/main/java/com/github/runly/richedittext/utils/ContextUtils.java
@@ -2,25 +2,27 @@ package com.github.runly.richedittext.utils;
 
 import android.content.Context;
 
+import java.lang.ref.WeakReference;
+
 /**
  * @author liye
  * @version 4.1.0
  * @since: 16/4/20 下午3:30
  */
 public class ContextUtils {
-    private static Context sContext;
+    private static WeakReference<Context> mContext;
 
     /**
      * @param context context
      */
     public static void setContext(Context context) {
-        sContext = context;
+        mContext = new WeakReference<>(context);
     }
 
     /**
      * @return Context
      */
     public static Context getContext() {
-        return sContext;
+        return mContext.get();
     }
 }
