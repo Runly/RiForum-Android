b3a50ffb920aa7056c5d41a454964773e8190c6c
冉力 <ranly314@gmail.com>
2017-03-20 20:32:07 +0800

    no message

--- app/src/main/java/com/github/runly/riforum_android/ui/activity/MainActivity.java
+++ app/src/main/java/com/github/runly/riforum_android/ui/activity/MainActivity.java
@@ -115,7 +115,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
         pagerAdapter.addFragment(RecommendFrag.createInstance(), getString(R.string.tab_1));
         pagerAdapter.addFragment(ForumFrag.createInstance(), getString(R.string.tab_2));
-        pagerAdapter.addFragment(DiscoverFrag.createInstance(20), getString(R.string.tab_3));
+        pagerAdapter.addFragment(DiscoverFrag.createInstance(), getString(R.string.tab_3));
         viewPager.setAdapter(pagerAdapter);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
