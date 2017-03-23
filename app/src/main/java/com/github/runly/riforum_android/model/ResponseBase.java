8b34c9a2fadd7af214a1c3a298a3232957ce6672
冉力 <ranly314@gmail.com>
2017-03-23 11:11:38 +0800

    增加字段

--- app/src/main/java/com/github/runly/riforum_android/model/ResponseBase.java
+++ app/src/main/java/com/github/runly/riforum_android/model/ResponseBase.java
@@ -1,5 +1,7 @@
 package com.github.runly.riforum_android.model;
 
+import java.util.List;
+
 /**
  * Created by ranly on 17-2-14.
  */
@@ -9,4 +11,8 @@ public class ResponseBase<T> extends ModelBase {
     public String code;
     public long dateline;
     public T data;
+    public List<Entry> entry_list;
+    public List<User> user_list;
+    public int entry_number;
+    public int user_number;
 }
